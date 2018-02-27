e500b10319c07028aa412714cc299b8bb695c5a2
Luke Barrett <gotluke@gmail.com>
2018-02-26 21:15:27 -0700

    added video background

--- src/App/Main/index.js
+++ src/App/Main/index.js
@@ -4,6 +4,7 @@ import Local from '../images/localEvenings.png';
 import Full from '../images/fullstack.png';
 import styled from 'styled-components';
 import Canvas from '../Canvas/index';
+import Typing from '../images/typing.mp4';
 
 const ImgWrapper = styled.div`
     display: flex;
@@ -11,47 +12,53 @@ const ImgWrapper = styled.div`
     width: 100%;
     
 `
-const Img = styled.img`
-    height: 150px;
-    width: 150px;
+const Videocont = styled.div`
+    position: fixed;
+    z-index: -1;
 `
-const Content = styled.div`
-    padding: 10px;
-    &:hover ${Img} {
-        height: auto;
-        width: 800px;
-        
-    }
-`
-const Body = styled.div`
-    height: 100vh;
-    background: linear-gradient(to right, black, black, black, red, yellow);
+const Video = styled.video`
+    height: auto;
+    vertical-align: middle;
+    width: 100%;
+    
 `
 
-const Heading = styled.h2`
-    color: gray;
+const Body = styled.div`
+    height: 100vh;
+    width: 100vw;
+    
+`
+
+const Overlay = styled.div`
+    display: grid;
+    height: 100vh;
+    
+    
+    
+    color: white;
+    
+`
+const Name = styled.h1`
+    grid-column: 2 / 3;
+    grid-row: 2 /3;
+    font-size: 9vw;
+    
+    justify-content: center;
 `
 
 function Main() {
     return (
         <div>
-        <Body>
-        <Canvas />
-        <ImgWrapper>
-            <Content>
-                <Heading>Workout App</Heading>
-                <Img src={Workout} alt="Workout app" />
-            </Content>
-            <Content>
-                <Heading>Buisness site</Heading>
-                <Img src={Local} alt="HTML and styling app" />
-            </Content>
-            <Content>
-                <Heading>Full Stack</Heading>
-                <Img src={Full} alt="Full stack site" />
-            </Content>
-        </ImgWrapper>
-        </Body>
+            {/* <Body> */}
+            <Videocont>
+                <Video video autobuffer loop autoPlay>
+                    <source src={Typing} type="video/mp4" />
+                </Video>
+            </Videocont>
+            <Overlay>
+                <Name>Luke Barrett</Name>
+            </Overlay>
+            {/* </Body> */}
         </div>
     )
 }
