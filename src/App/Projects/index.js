2fbb0b38bcfd4686300d72446a8d9a984ec5ff1f
Luke Barrett <gotluke@gmail.com>
2018-03-14 20:38:35 -0600

    update on styling buttons

--- src/App/Projects/index.js
+++ src/App/Projects/index.js
@@ -10,17 +10,38 @@ const Wrapper = styled.section`
     background: rgba(49, 49, 48, 0.5);
 `;
 const Project = styled.div`
-    display: flex;
+    display: grid;
+    grid-template-columns: auto 1fr;
     padding-top: 30px;
     width: 70vw; 
 `;
 const Link = styled.div`
     display: grid;
-    grid-template-rows: auto 30px 30px;
+    grid-template-rows: 28px 20px auto auto auto;
     margin-left: 20px;
 `;
 const Heading = styled.h1`
     margin-top: 20px;
+`;
+const Tag = styled.a`
+    text-decoration: none;
+    margin 10px;
+    background: linear-gradient(to bottom, rgba(149,149,149,1) 0%,rgba(13,13,13,1) 46%,rgba(1,1,1,1) 50%,rgba(10,10,10,1) 53%,rgba(78,78,78,1) 76%,rgba(56,56,56,1) 87%,rgba(27,27,27,1) 100%);
+    color: white;
+    width: 35px;
+    height: 20px;
+    text-align: center;
+    border-radius: 4px;
+        :active{
+            text-decoration: none;
+            margin 10px;
+            background: linear-gradient(to bottom, rgba(255,255,255,1) 0%,rgba(241,241,241,1) 50%,rgba(225,225,225,1) 51%,rgba(246,246,246,1) 100%);
+            color: black;
+            width: 35px;
+            height: 20px;
+            text-align: center;
+            border-radius: 4px;
+        }
 `;
 
 function Projects() {
@@ -30,34 +51,29 @@ function Projects() {
             <Project>
                 <img src={Local} alt="Pic of frontend website" />
                 <Link>
+                    <h1>Local Evenings</h1>
+                    <h5>HTML, CSS</h5>
                     <p>This is built with HTML & CSS using visual studio code. The most challanging part of building this was linking to the diffrent pages. The best part of building this was the styling.</p>
-                    <form>
-                        <button formAction="https://confident-mestorf-46653c.netlify.com/">Site</button>
-                    </form>
-                    <form>
-                        <button formAction="https://github.com/brickmaker17/business-time">Github</button>
-                    </form>
+                    <Tag role="button" href="https://confident-mestorf-46653c.netlify.com/">Site</Tag>
+                    <Tag role="button" href="https://github.com/brickmaker17/business-time">Git</Tag>
                 </Link>
             </Project>
             <Project>
                 <img src={Workout} alt="Pic of group full stack project" />
                 <Link>
+                    <h1>Fit</h1>
+                    <h5>HTML, CSS, JAVASCRIPT, REACT and REDUX</h5>
                     <p>This is built with reactjs, readux, javascript, HTML & CSS. This was a group project that used github.</p>
-                    <form>
-                        <button formAction="">Github</button>
-                    </form>
+                    <Tag role="button" href="">Git</Tag>
                 </Link>
             </Project>
             <Project>
                 <img src={Full} alt="Pic of full stack personal project" />
                 <Link>
+                    <h1>Local Evenings</h1>
                     <p>This if a fully C.R.U.D. personal project built with reactjs.</p>
-                    <form>
-                        <button formAction="https://suspicious-heyrovsky-bc9347.netlify.com/">Site</button>
-                    </form>
-                    <form>
-                        <button formAction="https://github.com/brickmaker17/local-evenings">Github</button>
-                    </form>
+                    <Tag role="button" href="https://suspicious-heyrovsky-bc9347.netlify.com/">Site</Tag>
+                    <Tag role="button" href="https://github.com/brickmaker17/local-evenings">Git</Tag>
                 </Link>
             </Project>
         </Wrapper>
